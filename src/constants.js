e425baa267ab641a6153be64e19a404ec32c666d
Rohit Gilbile <rgilbile@officebrain.com>
2017-11-16 11:17:55 +0530

    Intial commit

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
 export const baseUrl = "http://localhost:8082/"
 export const apiUrl = "http://172.16.60.128:3030/";
 export const imageProcessingUrl = "http://172.16.60.128:6789/";
+
 export const userUploadeImageUrl = "http://172.16.60.128:6789/user-uploaded-images/";
+
 
 // export default {
 //    baseUrl : "http://localhost:8082/"
