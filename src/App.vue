cdc340238e875a38b7e742a7d5d2d15cf9fda321
Rohit Gilbile <rgilbile@officebrain.com>
2017-11-17 10:49:15 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,14 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
-
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app {font-family: 'Avenir', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;text-align: center;color: #2c3e50; margin-top: 60px;}
 </style>
