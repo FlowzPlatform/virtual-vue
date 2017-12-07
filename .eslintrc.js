dbf01735aa1913656f63a889d8b459075fe78702
rahulob <rpatel@officebrain.com>
2017-12-07 13:30:53 +0530

    continue

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,3 @@
-// module.exports = {
-//   root: true,
-//   parser: 'babel-eslint',
-//   parserOptions: {
-//     sourceType: 'module'
-//   },
-//   env: {
-//     browser: true,
-//   },
-//   extends: 'standard',
-//   'rules': {
-//     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-//   }
-//  }
-
-
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
